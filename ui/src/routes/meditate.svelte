1c2aaf9a213f7e7f8ece2f13729215b988ecaad0
Alex Pirciu <alex.pirciu@zohomail.eu>
2022-06-11 04:41:47 +0300

    Add content on meditate page

--- ui/src/routes/meditate.svelte
+++ ui/src/routes/meditate.svelte
@@ -1,1 +1,56 @@
-<h1>This page is not available yet... Check back soon</h1>
+<script lang="ts">
+  import { fade } from "svelte/transition";
+  import Button from "$lib/components/Button.svelte";
+
+  let timerMinutes = "05";
+  let timerSeconds = "00";
+  let hasStarted = false;
+  let hasFinished = false;
+  let showText = false;
+
+  const timer = () => {
+    hasStarted = true;
+    var timer = setInterval(() => {
+      let actualMinutes = Number.parseInt(timerMinutes);
+      let actualSeconds = Number.parseInt(timerSeconds);
+      if (actualMinutes == 0 && actualSeconds == 1) {
+        timerMinutes = "05";
+        timerSeconds = "00";
+        clearInterval(timer);
+        hasFinished = true;
+        return;
+      }
+      if (actualSeconds === 0) {
+        actualSeconds = 59;
+        actualMinutes--;
+      } else {
+        actualSeconds--;
+      }
+
+      timerSeconds = `${
+        actualSeconds < 10 ? "0" + actualSeconds : actualSeconds
+      }`;
+      timerMinutes = `${
+        actualMinutes < 10 ? "0" + actualMinutes : actualMinutes
+      }`;
+    }, 1000);
+  };
+
+  $: {
+    showText = hasStarted && hasFinished;
+  }
+</script>
+
+<div class="flex flex-col justify-center items-center">
+  <h4>Start your meditation session</h4>
+  <p class="mb-6 bg-dark text-light p-14 rounded-[50%] text-xl">
+    {timerMinutes}:{timerSeconds}
+  </p>
+  <Button onClick={timer}>start</Button>
+</div>
+
+{#if showText}
+  <h5 class="mt-10" transition:fade>
+    Hooray, you've finised meditating for today!
+  </h5>
+{/if}
